32489fd5e41a6e81d1d76d59ade0e22a2a9e60fb
Your Name <your_email@example.com>
2025-05-31 14:49:56 +0530

    Remove service worker completely

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import * as serviceWorkerRegistration from "./service-worker";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -11,9 +10,6 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// Disable service worker to prevent build issues
-serviceWorkerRegistration.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
